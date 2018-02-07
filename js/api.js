6a1da7f0d0eae3aa1f561c300728fd1c822004f7
liliwaukee <lilimmvisual@gmail.com>
2018-02-07 17:16:00 +0000

    agregando vista partidos

---
+++ js/api.js
@@ -0,0 +1,8 @@
+
+/*
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://yesno.wtf/api/");
+xhr.send();*/
+
+var api = "https://yesno.wtf/api/";
+console.log(api);
--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-
+$(document).ready(function(){
+   $('ul.tabs').tabs('select_tab', 'tab_id');
+   $(".button-collapse").sideNav();
+   $('.collapsible').collapsible();
+   
+ });
